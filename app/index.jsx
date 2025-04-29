fbc7ea3821c0bbeb6f447518e055b7ab0f7c19af
Jenny Digné <jenny_digne@hotmail.com>
2025-04-29 22:17:32 +0200

    ändrat till mp4-bakgrund i index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,5 @@
-import { Text, View, Image } from "react-native";
-import { Link } from "expo-router";
+import { View, Image, StyleSheet } from "react-native";
+import { Video } from "expo-av";
 import MyButton from "../.expo/components/Button";
 import { useRouter } from "expo-router";
 
@@ -7,29 +7,40 @@ export default function HomeScreen() {
   const router = useRouter();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 60,
-        backgroundColor: "#fed9b7"
-      }}
-    >
-      <View>
-        <Image source={require('../assets/images/LogoText.png')} style={{ width: 195, height: 50.9 }} />
-      </View>
-      <View
-        style={{
-          gap: 50
-        }}>
-        <MyButton title="Sign in"
-          onPress={() => router.push('/login')} />
-        <MyButton title="Sign up"
-          onPress={() => router.push('/signup')} />
+    <View style={styles.container}>
+      <Video
+        source={require('../assets/videos/gradient-start.mp4')}
+        style={StyleSheet.absoluteFill}
+        resizeMode="cover"
+        isLooping
+        shouldPlay
+        isMuted
+      />
+      <View style={styles.content}>
+        <Image
+          source={require("../assets/images/LogoText.png")}
+          style={{ width: 195, height: 50.9 }}
+        />
+
+        <View style={{ gap: 50 }}>
+          <MyButton title="Sign in" onPress={() => router.push("/login")} />
+          <MyButton title="Sign up" onPress={() => router.push("/signup")} />
+        </View>
       </View>
     </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "relative",
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 60,
+    paddingHorizontal: 20,
+  },
+});
