23b06555313697bb4830551d913c717525e00534
Jenny Digné <jenny_digne@hotmail.com>
2025-05-14 14:56:19 +0200

    skapat fil som hämtar användarens profi från db

---
+++ getUserProfile.js
@@ -0,0 +1,21 @@
+//Hämtar användarens färdiga profil från databasen
+
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./firebaseConfig";
+
+export const getUserProfile = async (uid) => {
+  try {
+    const profileRef = doc(db, "profiles", uid, "profile");
+    const profileSnap = await getDoc(profileRef);
+
+    if (profileSnap.exists()) {
+      return profileSnap.data();
+    } else {
+      console.log("No profile found");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching profile:", error);
+    return null;
+  }
+};
