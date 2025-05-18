dfe13915cc7c3698256f4d54e817c53c3c92b0e0
Jenny Digné <jenny_digne@hotmail.com>
2025-05-18 16:42:13 +0200

    lagt till en shadow i shadows.js

--- styles/shadows.js
+++ styles/shadows.js
@@ -4,10 +4,22 @@ export const defaultShadow = Platform.select({
     ios: {
         shadowColor: "#000",
         shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
+        shadowOpacity: 0.2,
         shadowRadius: 4,
     },
     android: {
         elevation: 3,
+    },
+});
+
+export const navShadow = Platform.select({
+    ios: {
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+    },
+    android: {
+        elevation: 10,
     },
 });
